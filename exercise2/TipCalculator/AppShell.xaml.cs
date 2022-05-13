5e09aaebdd7cab435934828636fa7159b0cb0339
James Montemagno <james.montemagno@gmail.com>
2022-05-13 11:20:11 -0700

    Add base for e2

---
+++ exercise2/TipCalculator/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TipCalculator;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+
+        Routing.RegisterRoute(nameof(CustomTipPage), typeof(CustomTipPage));
+    }
+}
